c4c65f83c285378956b830011604b1f1bbb5467f
bezrukavyi <yaroslav.bezrukavyi@gmail.com>
2019-03-03 13:19:11 +0200

    Add flash render | Fix create service

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,28 +4,20 @@ class LineItemsController < ApplicationController
   check_user_order
 
   def create
-    @result = CartService::LineItem::Create.call(current_order, **create_params)
+    @result = CartService::LineItem::Create.call(current_order, create_params)
 
-    if @result.success?
-      redirect_to :back
-    else
-      redirect_with_error :back, @result.value
-    end
+    redirect_back_result(@result)
   end
 
   def destroy
     @result = CartService::LineItem::Delete.call(current_order, params[:product_id])
 
-    if @result.success?
-      redirect_to :back
-    else
-      redirect_with_error :back, @result.value
-    end
+    redirect_back_result(@result)
   end
 
   private
 
   def create_params
-    params.permit(:product_id, :quantity)
+    params.require(:line_item).permit(:product_id, :quantity)
   end
 end
